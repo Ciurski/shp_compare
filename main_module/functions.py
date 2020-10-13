9ebd98025f41c95080c7a6c0730ee7aad1350c6b
Ciurski <s.bidowaniec@gmail.com>
2020-10-13 20:32:26 +0200

    Compare functions concept. Based on shp header compare.

--- main_module/functions.py
+++ main_module/functions.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import fiona
+
 
 def list_all_shp(path):
     shapes = []
@@ -9,3 +11,93 @@ def list_all_shp(path):
             if ".shp" in file[-4:]:
                 shapes.append(os.path.join(root, file))
     return shapes
+
+
+def list_shp_fields(shp_list):
+    headers = set()
+    field_names = set()
+    field_params = {}
+    incompatibilities = {}
+    for shp in shp_list:
+        with fiona.open(shp, "r") as source:
+            shp_schema = source.schema
+
+        fields = shp_schema['properties']
+
+        table = []
+        for field in fields.keys():
+            field_names.add(field)
+            table.append(field)
+
+            if field in field_params.keys():
+                if field_params[field] != fields[field]:
+                    incompatibilities_list = incompatibilities.get(field, [])
+                    incompatibilities_list.append((field_params[field], fields[field]))
+                    incompatibilities[field] = incompatibilities_list
+
+            else:
+                field_params[field] = fields[field]
+        headers.add(tuple(table))
+
+    return headers, field_names, field_params, incompatibilities
+
+def compare_list_index(list_one, list_two):
+    res = []
+    for i, element in enumerate(list_two):
+        index = list_one.index(element)
+        res.append((i, index))
+    return tuple(res)
+
+def compare_headers(headers_patern, headers_to_check):
+    for check_header in headers_to_check:
+        if check_header not in headers_patern:
+            check_header_set = set(check_header)
+            for patern in headers_patern:
+                error_type = "Not_found"
+                patern_set = set(patern)
+                # if same set
+                if (check_header_set == patern_set):
+                    new_index = compare_list_index(patern, check_header)
+                    error_type = "order"
+
+                    # order
+                # if subset
+                elif check_header_set.issubset(patern_set):
+                    error_type = "subset"
+                    # bad name
+                    # bad type
+                    # not enough fields
+                # if superset
+                elif check_header_set.issuperset(patern_set):
+                    error_type = "superset"
+                    # to many fields
+                # partial match
+                else:
+                    z = check_header_set.intersection(patern_set)
+                    if len(z)>0:
+                        pass
+                # if none not match
+
+            return error_type
+    pass
+
+
+def compare_field_names(names_patern, names_to_check):
+    pass
+
+
+def compare_field_params(params_patern, params_to_check):
+    pass
+
+
+def unify_shp(shp, headers_patern, names_patern, params_patern):
+    pass
+
+if __name__ == '__main__':
+    path = os.path.dirname(os.getcwd()) + "/tests/files/headers_difer/order/pattern_one"
+    shp_list_one = list_all_shp(path)
+    headers_one, field_names_one, field_params_one, incompatibilities_one = list_shp_fields(shp_list_one)
+    path = os.path.dirname(os.getcwd()) + "/tests/files/headers_difer/order/pattern_two"
+    shp_list_two = list_all_shp(path)
+    headers_two, field_names_two, field_params_two, incompatibilities_two = list_shp_fields(shp_list_two)
+    compare_headers(headers_one, headers_two)
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,10 +2,78 @@
 
 import unittest
 import os
-from main_module.functions import list_all_shp
+from main_module.functions import list_all_shp, list_shp_fields, compare_headers
 
 class TestListAllShp(unittest.TestCase):
     def test_listing(self):
         path = os.getcwd()+"/files/list_test"
         shp_list = list_all_shp(path)
         self.assertEqual(len(shp_list), 4)
+
+
+class TestListShpFields(unittest.TestCase):
+    def test_fields(self):
+        path = os.getcwd()+"/files/list_test"
+        shp_list = list_all_shp(path)
+        headers, field_names, field_params, incompatibilities = list_shp_fields(shp_list)
+        self.assertEqual(len(incompatibilities), 0)
+
+    def test_incompatibilities(self):
+        path = os.getcwd()+"/files/list_test"
+        shp_list = list_all_shp(path)
+        headers, field_names, field_params, incompatibilities = list_shp_fields(shp_list)
+        self.assertEqual(len(incompatibilities), 0)
+
+
+class TestCompareHeaders(unittest.TestCase):
+    def test_order(self):
+        path = os.path.dirname(os.getcwd()) + "/tests/files/headers_differ/order/pattern_one"
+        shp_list_one = list_all_shp(path)
+        headers_one, field_names_one, field_params_one, incompatibilities_one = list_shp_fields(shp_list_one)
+        path = os.path.dirname(os.getcwd()) + "/tests/files/headers_differ/order/pattern_two"
+        shp_list_two = list_all_shp(path)
+        headers_two, field_names_two, field_params_two, incompatibilities_two = list_shp_fields(shp_list_two)
+        error = compare_headers(headers_one, headers_two)
+        self.assertEqual(error, "order")
+
+    def test_subset(self):
+        path = os.path.dirname(os.getcwd()) + "/tests/files/headers_differ/subset/pattern_one"
+        shp_list_one = list_all_shp(path)
+        headers_one, field_names_one, field_params_one, incompatibilities_one = list_shp_fields(shp_list_one)
+        path = os.path.dirname(os.getcwd()) + "/tests/files/headers_differ/subset/pattern_two"
+        shp_list_two = list_all_shp(path)
+        headers_two, field_names_two, field_params_two, incompatibilities_two = list_shp_fields(shp_list_two)
+        error = compare_headers(headers_one, headers_two)
+        self.assertEqual(error, "subset")
+
+    def test_superset(self):
+        path = os.path.dirname(os.getcwd()) + "/tests/files/headers_differ/superset/pattern_one"
+        shp_list_one = list_all_shp(path)
+        headers_one, field_names_one, field_params_one, incompatibilities_one = list_shp_fields(shp_list_one)
+        path = os.path.dirname(os.getcwd()) + "/tests/files/headers_differ/superset/pattern_two"
+        shp_list_two = list_all_shp(path)
+        headers_two, field_names_two, field_params_two, incompatibilities_two = list_shp_fields(shp_list_two)
+        error = compare_headers(headers_one, headers_two)
+        self.assertEqual(error, "superset")
+
+    def test_bad_name(self):
+        path = os.getcwd()+"/files/headers_differ/bad_name/pattern_one"
+        shp_list_one = list_all_shp(path)
+        headers_one, field_names_one, field_params_one, incompatibilities_one = list_shp_fields(shp_list_one)
+        path = os.getcwd()+"/files/headers_differ/bad_name/pattern_two"
+        shp_list_two = list_all_shp(path)
+        headers_two, field_names_two, field_params_two, incompatibilities_two = list_shp_fields(shp_list_two)
+        compare_headers(headers_one, headers_two)
+
+    def test_bad_type(self):
+        path = os.getcwd() + "/files/headers_differ/bad_type/pattern_one"
+        shp_list_one = list_all_shp(path)
+        headers_one, field_names_one, field_params_one, incompatibilities_one = list_shp_fields(shp_list_one)
+        path = os.getcwd() + "/files/headers_differ/bad_type/pattern_two"
+        shp_list_two = list_all_shp(path)
+        headers_two, field_names_two, field_params_two, incompatibilities_two = list_shp_fields(shp_list_two)
+        compare_headers(headers_one, headers_two)
+
+
+if __name__ == '__main__':
+    unittest.main()
